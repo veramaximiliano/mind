3c2a29cab7432618a0100cf661c3cc5c98fcc91b
veramaximiliano <vera.maximiliano@gmail.com>
2025-04-01 21:18:02 -0300

    feat: Modify color palettes

--- src/components/ThemeSwitch/index.jsx
+++ src/components/ThemeSwitch/index.jsx
@@ -3,16 +3,16 @@ import './styles.css'
 
 export default function ThemeSwitch({theme, onChangeTheme}) {
     const [active, setActive] = useState(false)
-    const themes = ['light', 'dark', 'pink', 'green', 'yellow']
+    const themes = ['light', 'dark', 'pink', 'bb-blue', 'yellow']
 
     return (
         <div className={`theme-switch${active ? ' active' : ''}`}>
             <div className={`theme-switch-option theme-switch-option-${theme}`} onClick = {() => setActive(!active)}>theme-{theme}</div>
             <ul className="theme-switch-options">
             {
-                themes.map((t, index) => (
+                themes.map((t, index) => 
                     t === theme ? (<></>) : (<li key={index} className={`theme-switch-option theme-switch-option-${t}`} onClick = { () => { onChangeTheme(t); setActive(false)} }>theme-{t}</li>)
-                ))
+                )
             }
             </ul>
         </div>
